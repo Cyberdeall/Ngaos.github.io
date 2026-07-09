e25ecd5f8aa007393215242dc21e559ef77b808a
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-09 17:52:09 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 };
 const CONFIG = {
-    APP_NAME: "AFS NDEREK NGAOS YAI",
+    APP_NAME: "NGAOS AL FALAH PLOSO",
     APP_DESC: "TAFSIR JALALAIN DAN SHAHIH BUKHARI",
     STREAM_URL: "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
     SESSION_HOURS: 4
