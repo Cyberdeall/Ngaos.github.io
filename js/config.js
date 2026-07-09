2e4386ba456e895ebf9c6b66d9d6e27dc2ba5ab5
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-09 22:46:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,17 @@
-};
+// =========================================
+// CONFIG.JS
+// Final Version 1.0
+// =========================================
+
 const CONFIG = {
+
     APP_NAME: "NGAOS AL FALAH PLOSO",
+
     APP_DESC: "TAFSIR JALALAIN DAN SHAHIH BUKHARI",
-    STREAM_URL: "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
+
+    STREAM_URL:
+        "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
+
     SESSION_HOURS: 4
+
 };
