5e5d7cdaf2bf27c69b859b19298ed6239b4ae22c
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-10 03:34:39 +0700

    Update users.js

--- js/users.js
+++ js/users.js
@@ -4,9 +4,10 @@
 
 const USERS = [
 {
-id:1,
-username:"PPTAALFALAH",
-passwordHash:"a0864e3ebd760eea3c1c4a60d232f47d3294f6978bcc6ac8b8dc89f4394de793",
-role:"user",
-active:true
-},
+    id:1,
+    username:"PPTAALFALAH",
+    passwordHash:"a0864e3ebd760eea3c1c4a60d232f47d3294f6978bcc6ac8b8dc89f4394de793",
+    role:"user",
+    active:true
+}
+];
