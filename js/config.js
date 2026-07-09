227f3cd75b2f8062f6f70a14c4545d1356ee1b4d
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-09 17:32:36 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// ================================
+// RADIO CONFIGURATION
+// ================================
+
+const CONFIG = {
+
+    APP_NAME: "AFS NDEREK NGAOS YAI",
+
+    APP_DESC: "TAFSIR JALALAIN",
+
+    STREAM_URL:
+    "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
+
+    VERSION: "1.0.0"
+
+};
