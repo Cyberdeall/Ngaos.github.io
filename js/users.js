af7957e3fd89804f3b41cd6c8385403da5a1f71c
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-09 12:44:10 +0700

    Create users.js

---
+++ js/users.js
@@ -0,0 +1,14 @@
+// ===============================
+// DATABASE USER
+// ===============================
+
+const USERS = [
+
+{
+    username: "admin",
+
+    password:
+    "240be518fabd2724ddb6f04eebcdbf1d75b21f5d5f5f7f4d4f8f9e5d7c0b7c56"
+},
+
+];
