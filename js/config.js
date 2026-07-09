a8ba3ee1dcccba55fdf1e310ed969487758fac9c
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-09 17:49:52 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,16 +1,7 @@
-// ================================
-// RADIO CONFIGURATION
-// ================================
-
+};
 const CONFIG = {
-
     APP_NAME: "AFS NDEREK NGAOS YAI",
-
-    APP_DESC: "TAFSIR JALALAIN",
-
-    STREAM_URL:
-    "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
-
-    VERSION: "1.0.0"
-
+    APP_DESC: "TAFSIR JALALAIN DAN SHAHIH BUKHARI",
+    STREAM_URL: "https://uk14freenew.listen2myradio.com/live.mp3?typeportmount=s1_27764_stream_367452469",
+    SESSION_HOURS: 4
 };
