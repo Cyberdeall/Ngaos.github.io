d7c599075aae85c7fd14a26e218e81148adef7d9
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-17 01:04:14 +0700

    Update users.js

--- js/users.js
+++ js/users.js
@@ -7,12 +7,12 @@ const USER_DATA = Object.freeze({
         {
             USERNAME: "AFS",
             PASSWORD_HASH: "854d19cf6ced5bc4b7833ce07be41878d7e31cadefd9639c1c18575fb0372a98", // apabila status tidak aktif tuliskan "NONAKTIF"
-            STATUS: "AKTIF" // Akun ini bisa digunakan untuk login
+            STATUS: "AKTIF" 
         },
         {
             USERNAME: "PPTA",
             PASSWORD_HASH: "a0864e3ebd760eea3c1c4a60d232f47d3294f6978bcc6ac8b8dc89f4394de793", // 
-            STATUS: "AKTIF" // Akun ini dikunci/dinonaktifkan (Otomatis ditolak saat login)
+            STATUS: "AKTIF"
         }
         // Silakan tambah baris baru di bawah ini menggunakan kode hijau dari generator.html. jangan lupa tanda koma setelah kurung kurawal apabila masih ada user di bawahnya.
     ]
