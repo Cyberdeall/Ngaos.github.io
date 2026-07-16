22e4c7714422da12bf87da1164dc0db3ace6a0e8
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-16 23:55:30 +0700

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,13 +1,9 @@
-// ======================================
-// USERS DATABASE
-// ======================================
-
-const USERS = [
-{
-    id:1,
-    username:"PPTAALFALAH",
-    passwordHash:"a0864e3ebd760eea3c1c4a60d232f47d3294f6978bcc6ac8b8dc89f4394de793",
-    role:"user",
-    active:true
-}
-];
+// =========================================================================
+// USERS.JS - Basis Data Kredensial Pengguna Terenkripsi SHA-256 Hash
+// =========================================================================
+const USER_DATA = Object.freeze({
+    // Username: PPTAALFALAH
+    // Password: PPTAALFALAH (Hasil hash SHA-256 dicantumkan di bawah ini)
+    USERNAME: "PPTAALFALAH",
+    PASSWORD_HASH: "cc2cc8e2501a4fbda1a1f9e2b173523fc6c39b6f376cfb940026b91176b6b772"
+});
