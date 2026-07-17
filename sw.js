e5078ea11c7308bdbb80107bf251c38f5e1045a0
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-18 06:22:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'ngaos-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './css/login.css',
+  './logo-alfalah.png'
 ];
 
-// Pemasangan Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Strategi Cache: Mencoba jaringan dulu, jika gagal/offline ambil dari cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
-
