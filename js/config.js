7cfd9693be21791229434f58aa275461451f170d
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-22 02:13:15 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,43 +1,29 @@
 // =========================================
 // CONFIG.JS
-// Final Version 3.0
+// Version 4.0 - Clerk Integration
 // =========================================
 
 const CONFIG = Object.freeze({
 
-    // ============================
     // Informasi Aplikasi
-    // ============================
-
     APP_NAME: "NGAOS AL FALAH PLOSO",
-
     APP_DESC: "TAFSIR JALALAIN DAN SHAHIH BUKHARI",
+    APP_VERSION: "4.0.0",
 
-    APP_VERSION: "3.0.0",
-
-    // ============================
     // Audio Stream
-    // ============================
+    STREAM_URL: "https://alhastream.com",
 
-    STREAM_URL:
-        "https://alhastream.com",
+    // Clerk Authentication
+    // PASTE PUBLISHABLE KEY CLERK ANDA DI SINI
+    CLERK_PUBLISHABLE_KEY: "pk_test_ZnVuLXBpZ2Vvbi02Mi5jbGVyay5hY2NvdW50cy5kZXYk",
 
-    // ============================
-    // Session
-    // ============================
-
+    // Session & Cache
     SESSION_HOURS: 4,
-
     SESSION_KEY: "radio_session",
-
     REMEMBER_KEY: "remember_username",
 
-    // ============================
-    // Redirect
-    // ============================
-
+    // Redirect Pages
     LOGIN_PAGE: "index.html",
-
     PLAYER_PAGE: "player.html"
 
 });
