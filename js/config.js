a9b3f80ec37a10594528bf9a1c286642500da76b
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-24 13:03:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,11 +10,10 @@ const CONFIG = Object.freeze({
     APP_DESC: "TAFSIR JALALAIN DAN SHAHIH BUKHARI",
     APP_VERSION: "4.0.0",
 
-    // Audio Stream
-    STREAM_URL: "https://b.alhastream.com:5125/radio",
+    // Audio Stream (Format URL Icecast)
+    STREAM_URL: "https://alhastream.com/radio",
 
-    // Clerk Authentication
-    // PASTE PUBLISHABLE KEY CLERK ANDA DI SINI
+    // Clerk Authentication Publishable Key
     CLERK_PUBLISHABLE_KEY: "pk_test_ZnVuLXBpZ2Vvbi02Mi5jbGVyay5hY2NvdW50cy5kZXYk",
 
     // Session & Cache
