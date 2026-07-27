98ac20d565782d1fc212f8c74ff8bc0e5ced9a07
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-28 00:14:26 +0700

    Create en.js

---
+++ js/i18n/en.js
@@ -0,0 +1,13 @@
+export default {
+  "app_title": "Ngaos App",
+  "welcome": "Welcome to Ngaji App",
+  "hero_subtitle": "Listen to the recitation of the Holy Qur'an anytime.",
+  "login": "Sign In",
+  "register": "Sign Up",
+  "logout": "Sign Out",
+  "play": "Play",
+  "pause": "Pause",
+  "search_placeholder": "Search surah or recitation...",
+  "no_track_selected": "No audio selected",
+  "greeting_user": "Hello, {name}"
+};
