aa0957213fd5cf2ae391cf2316261cb2a08d4365
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-28 00:36:06 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Konfigurasi terpusat untuk kemudahan migrasi server/auth di masa depan
 export const config = {
-  CLERK_PUBLISHABLE_KEY: 'pk_test_YOUR_CLERK_KEY_HERE', // Masukkan Key asli Anda
-  AUDIO_SERVER_URL: 'https://api.quran.com/api/v4',      // URL Server Audio
+  CLERK_PUBLISHABLE_KEY: 'pk_test_ZnVuLXBpZ2Vvbi02Mi5jbGVyay5hY2NvdW50cy5kZXYk', // Masukkan Key asli Anda
+  AUDIO_SERVER_URL: 'https://b.alhastream.com:5125/radio,      // URL Server Audio
   DEFAULT_LANG: 'id'
 };
 
