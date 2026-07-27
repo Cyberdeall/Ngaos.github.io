02563ac660741858d6da36876034b1fde6f4d32e
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-28 00:25:27 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Konfigurasi terpusat untuk kemudahan migrasi server/auth di masa depan
-export const config = {
-  CLERK_PUBLISHABLE_KEY: 'pk_test_YOUR_CLERK_KEY_HERE', // Masukkan Key asli Anda
-  AUDIO_SERVER_URL: 'https://api.quran.com/api/v4',      // URL Server Audio
-  DEFAULT_LANG: 'id'
-};
