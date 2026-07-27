bead4e23964dcd020c1dd4cdfec6e44b23718120
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-28 00:27:59 +0700

    Create id.js

---
+++ js/i18n/id.js
@@ -0,0 +1,14 @@
+export default {
+  "app_title": "Ngaos App",
+  "welcome": "Selamat Datang di Aplikasi Ngaji",
+  "hero_subtitle": "Dengarkan lantunan ayat suci Al-Qur'an kapan saja.",
+  "login": "Masuk",
+  "register": "Daftar",
+  "logout": "Keluar",
+  "play": "Putar",
+  "pause": "Jeda",
+  "search_placeholder": "Cari surah atau bacaan...",
+  "no_track_selected": "Belum ada audio dipilih",
+  "greeting_user": "Halo, {name}"
+};
+
